c2a1742ab17eccdb41a7ac5d4abbe4c622b7bbba
eunicemeye18 <eunicemeye18@gmail.com>
2026-08-09 03:30:27 +0100

    Modifications des pages d'accueils

--- lib/doctors/screens/disponibility_screen.dart
+++ lib/doctors/screens/disponibility_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:medicare_v2/widgets/FilterDropdown.dart';
 import 'package:medicare_v2/widgets/disponibility_doctor_card.dart';
-import 'package:medicare_v2/widgets/filter_by_date.dart';
 
 class DisponibilityScreen extends StatefulWidget {
   const DisponibilityScreen({super.key});
@@ -24,23 +24,96 @@ class _DisponibilityScreenState extends State<DisponibilityScreen> {
                   context,
                 ).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
               ),
-              FilterByDate(),
               Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(4.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      "Mardi",
-                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
+                    Filterdropdown(
+                      values: [
+                        "Cette Semaine",
+                        "Ce Mois",
+                        "Mois Prochain",
+                        "Choisir une date",
+                      ],
+                      hint: "Cette Semaine",
+                    ),
+                    SizedBox(height: 12),
+                    DisponibilityDoctorCard(
+                      textDay: "Lundi",
+                      textDate: "19 Août",
+                      textHourOne: "08:00 - 12:00",
+                      textHourTwo: "14:00 - 17:30",
+                    ),
+                    DisponibilityDoctorCard(
+                      textDay: "Mardi",
+                      textDate: "20 Août",
+                      textHourOne: "08:00 - 12:00",
+                      textHourTwo: "14:00 - 17:30",
+                    ),
+                    DisponibilityDoctorCard(
+                      textDay: "Mercredi",
+                      textDate: "21 Août",
+                      textHourOne: "08:00 - 12:00",
+                      textHourTwo: "",
+                    ),
+                    DisponibilityDoctorCard(
+                      textDay: "Jeudi",
+                      textDate: "22 Août",
+                      textHourOne: "08:00 - 12:00",
+                      textHourTwo: "14:00 - 17:30",
+                    ),
+                    DisponibilityDoctorCard(
+                      textDay: "Vendredi",
+                      textDate: "23 Août",
+                      textHourOne: "08:00 - 12:00",
+                      textHourTwo: "14:00 - 17:30",
+                    ),
+                    DisponibilityDoctorCard(
+                      textDay: "Samedi",
+                      textDate: "24 Août",
+                      textHourOne: "08:00 - 12:00",
+                      textHourTwo: "14:00 - 17:30",
+                    ),
+                    DisponibilityDoctorCard(
+                      textDay: "Dimanche",
+                      textDate: "25 Août",
+                      textHourOne: "08:00 - 12:00",
+                      textHourTwo: "14:00 - 17:30",
+                    ),
+                    // DisponibilityDoctorCard(text: '08:00 - 10:00'),
+                    // DisponibilityDoctorCard(text: '10:30 - 12:30'),
+                    // DisponibilityDoctorCard(text: '14:00 - 16:00'),
+                    // DisponibilityDoctorCard(text: '16:30 - 18:30'),
+                    SizedBox(height: 25),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(
+                            context,
+                          ).colorScheme.primary,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadiusGeometry.circular(12),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(width: 60),
+                            Icon(Icons.add, color: Colors.white),
+                            SizedBox(width: 4),
+                            Text(
+                              "Ajoutez un créneau",
+                              style: Theme.of(context).textTheme.bodyMedium
+                                  ?.copyWith(color: Colors.white),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                    DisponibilityDoctorCard(text: '08:00 - 10:00'),
-                    DisponibilityDoctorCard(text: '10:30 - 12:30'),
-                    DisponibilityDoctorCard(text: '14:00 - 16:00'),
-                    DisponibilityDoctorCard(text: '16:30 - 18:30'),
                   ],
                 ),
               ),
--- lib/widgets/disponibility_doctor_card.dart
+++ lib/widgets/disponibility_doctor_card.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
 class DisponibilityDoctorCard extends StatefulWidget {
-  final String text;
-  const DisponibilityDoctorCard({super.key, required this.text});
+  final String textDay;
+  final String textDate;
+  final String textHourOne;
+  final String textHourTwo;
+  const DisponibilityDoctorCard({
+    super.key,
+    required this.textDay,
+    required this.textDate,
+    required this.textHourOne,
+    required this.textHourTwo,
+  });
 
   @override
   State<DisponibilityDoctorCard> createState() =>
@@ -13,42 +22,51 @@ class _DisponibilityDoctorCardState extends State<DisponibilityDoctorCard> {
   bool isSwitched = true;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: TextButton(
-        onPressed: () {},
-        style: TextButton.styleFrom(
-          foregroundColor: Colors.black,
-          shape: LinearBorder(side: BorderSide(width: 2)),
-          side: BorderSide(width: 2),
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 1, color: Colors.grey.shade200),
         ),
-        child: Row(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [Text(widget.text), Text("2h00")],
-            ),
-            SizedBox(width: 40),
-            TextButton(
-              onPressed: () {},
-              child: isSwitched
-                  ? Text("Disponible", style: TextStyle(color: Colors.green))
-                  : Text("indisponible", style: TextStyle(color: Colors.grey)),
-            ),
-            Spacer(),
-            Switch(
-              value: isSwitched,
-              inactiveThumbColor: Colors.grey,
-              activeThumbColor: Colors.white,
-              activeTrackColor: Colors.green,
-              inactiveTrackColor: Colors.white70,
-              onChanged: (value) {
-                setState(() {
-                  isSwitched = value;
-                });
-              },
-            ),
-          ],
+        child: TextButton(
+          onPressed: () {},
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.black,
+            shape: LinearBorder(side: BorderSide(width: 2)),
+            side: BorderSide(width: 2),
+          ),
+          child: Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [Text(widget.textDay), Text(widget.textDate)],
+              ),
+              Spacer(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [Text(widget.textHourOne), Text(widget.textHourTwo)],
+              ),
+              // TextButton(
+              //   onPressed: () {},
+              //   child: isSwitched
+              //       ? Text("Disponible", style: TextStyle(color: Colors.green))
+              //       : Text("indisponible", style: TextStyle(color: Colors.grey)),
+              // ),
+              Spacer(),
+              Switch(
+                value: isSwitched,
+                inactiveThumbColor: Colors.grey,
+                activeThumbColor: Colors.white,
+                activeTrackColor: Colors.green,
+                inactiveTrackColor: Colors.white70,
+                onChanged: (value) {
+                  setState(() {
+                    isSwitched = value;
+                  });
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
